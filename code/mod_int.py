1ce8138a8229073900ce51646a9af6384d31f016
Sagnik Bhattacharya <sagnikdps@gmail.com>
2020-07-10 21:58:55 -0700

    mod integers implemented

--- code/mod_int.py
+++ code/mod_int.py
@@ -1,24 +1,28 @@
 from __future__ import annotations
-from dataclasses import FrozenInstanceError
+from dataclasses import dataclass, FrozenInstanceError
 from operator import add, sub, mul
 from typing import *
 
 BIG_PRIME = 2_147_483_647
 
 
+@dataclass(frozen=True)
 class ModInt:
-    def __init__(self, val: int, prime: int = BIG_PRIME) -> None:
-        self.val = val % prime
-        self.prime = prime
+    val: int
+    prime: int = BIG_PRIME
+
+    def __post_init__(self):
+        object.__setattr__(self, "val", self.val % self.prime)
 
     def inverse(self) -> ModInt:
-        return ModInt(0)
-    
-    def __setattr__(self, attr, val) -> None:
-        raise FrozenInstanceError()
+        def egcd(x: int, y: int) -> Tuple[int, int, int]:
+            if y == 0:
+                return (x, 1, 0)
+            else:
+                d, a, b = egcd(y, x % y)  # d = ax + by
+                return (d, b, a - (x // y) * b)
 
-    def __delattr__(self, _) -> None:
-        raise FrozenInstanceError()
+        return ModInt(egcd(self.prime, self.val)[2], prime=self.prime)
 
     def _gen_operation(
         int_op: Callable[[int, int], int], name: str
@@ -45,6 +49,16 @@ class ModInt:
     __rsub__ = __sub__
     __mul__ = _gen_operation(mul, "multiplication")
     __rmul__ = __mul__
+    
+    def __truediv__(self, other: Union[ModInt, int]) -> ModInt:
+        if isinstance(other, int):
+            other = ModInt(other, self.prime)
+        return self * other.inverse()
+
+    __rtruediv__ = __truediv__
+
+    def __repr__(self):
+        return f"ModInt(val={self.val}, prime={self.prime})"
 
     def __str__(self):
         return f"{self.val} (mod {self.prime})"
